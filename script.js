3445146c98b213f6a1bdb21215f2dadf917509c1
EvS444 <emanuelsacoman@gmail.com>
2022-11-16 11:25:54 -0300

    Botão do git animado em 3D

--- script.js
+++ script.js
@@ -111,4 +111,50 @@ document.querySelectorAll('.botao').forEach(item => {
        
    });
    
-});    
+}); 
+
+// efeito 3d
+
+const card = document.querySelector('.container');
+
+    card.addEventListener("mousemove", cardEffect);
+    card.addEventListener("mouseleave", cardBack);
+    card.addEventListener("mouseenter", cardEnter);
+
+    function cardEffect(event)
+    {
+        const cardWidth = card.offsetWidth;
+        const cardHeight = card.offsetHeight;
+        const centerX = card.offsetLeft + cardWidth/2;
+        const centerY = card.offsetTop + cardHeight/2;
+        const positionX = event.clientX - centerX;
+        const positionY = event.clientY - centerY;
+        
+        const rotateX = ((+1)*20*positionY/(cardHeight/2)).toFixed(2);
+        const rotateY = ((-1)*20*positionX/(cardWidth/2)).toFixed(2);
+
+        console.log(rotateX,rotateY);
+
+        card.style.transform = `perspective(500px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+
+    }
+
+    function cardBack(event)
+    {
+        card.style.transform = `perspective(500px) rotateX(0deg) rotateY(0deg)`;
+        cardTransition();
+    }
+
+    function cardTransition()
+    {
+        clearInterval(card.transitionId);
+        card.style.transition = 'transform 1000ms';
+        card.transitionId = setTimeout(() => {
+            card.style.transition = '';
+        },400);
+    }
+
+    function cardEnter(event)
+    {
+        cardTransition();
+    }
